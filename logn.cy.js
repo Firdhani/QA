1b124b3e61e7fb92f54d390617cc6453e3f71fee
Firdhani <firdhanis97@gmail.com>
2024-11-23 22:24:11 +0700

    Add files via upload

---
+++ logn.cy.js
@@ -0,0 +1,60 @@
+/// <referance types="cypress" />
+
+describe('Login Feature', () =>  {
+    it('User Login with valid credentials', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.get('h5').contains('Login').should('have.text','Login');
+        cy.get('[name="username"]').type('Admin');
+        cy.get('[name="password"]').type('admin123');
+        cy.get('[type="submit"]').click();
+        cy.get('h6').contains('Dashboard').should('have.text','Dashboard');
+    }) 
+})
+
+
+describe('Login Feature', () =>  {
+    it('User Login with invalid credentials', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.get('h5').contains('Login').should('have.text','Login');
+        cy.get('[name="username"]').type('Admina');
+        cy.get('[name="password"]').type('admin1234');
+        cy.get('[type="submit"]').click();
+        cy.get('p').contains('Invalid credentials').should('have.text','Invalid credentials');
+    }) 
+})
+
+
+describe('Login Feature', () =>  {
+    it('User Login with sensitve case', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.get('h5').contains('Login').should('have.text','Login');
+        cy.get('[name="username"]').type('admin');
+        cy.get('[name="password"]').type('admin123');
+        cy.get('[type="submit"]').click();
+        cy.get('h6').contains('Dashboard').should('have.text','Dashboard');
+    }) 
+})
+
+
+describe('Login Feature', () =>  {
+    it('User Login with invalid credentials', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.get('h5').contains('Login').should('have.text','Login');
+        cy.get('[name="username"]').type('Admin');
+        cy.get('[name="password"]').type('admin1234');
+        cy.get('[type="submit"]').click();
+        cy.get('p').contains('Invalid credentials').should('have.text','Invalid credentials');
+    }) 
+})
+
+
+describe('Login Feature', () =>  {
+    it('User Login with Required', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.get('h5').contains('Login').should('have.text','Login');
+        cy.get('[name="username"]').type('Admin');
+        cy.get('[name="password"]').clear();
+        cy.get('[type="submit"]').click();
+        cy.get('span').contains('Required').should('have.text','Required');
+    }) 
+})
